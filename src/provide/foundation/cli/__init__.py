8729f45d638a2f0307bccddabec520833634ca40
Tim <code@tim.life>
2025-08-31 20:47:44 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/provide/foundation/cli/__init__.py
+++ src/provide/foundation/cli/__init__.py
@@ -11,6 +11,8 @@ from provide.foundation.cli.decorators import (
     output_options,
     standard_options,
     error_handler,
+    pass_context,
+    version_option,
 )
 from provide.foundation.cli.utils import (
     echo_json,
@@ -20,6 +22,17 @@ from provide.foundation.cli.utils import (
     echo_info,
     setup_cli_logging,
     create_cli_context,
+    CliTestRunner,
+    assert_cli_success,
+    assert_cli_error,
+)
+from provide.foundation.cli.testing import (
+    MockContext,
+    isolated_cli_runner,
+    temp_config_file,
+    create_test_cli,
+    mock_logger,
+    CliTestCase,
 )
 
 __all__ = [
@@ -29,6 +42,8 @@ __all__ = [
     "output_options",
     "standard_options",
     "error_handler",
+    "pass_context",
+    "version_option",
     # Utilities
     "echo_json",
     "echo_error",
@@ -37,4 +52,14 @@ __all__ = [
     "echo_info",
     "setup_cli_logging",
     "create_cli_context",
+    # Testing
+    "CliTestRunner",
+    "assert_cli_success",
+    "assert_cli_error",
+    "MockContext",
+    "isolated_cli_runner",
+    "temp_config_file",
+    "create_test_cli",
+    "mock_logger",
+    "CliTestCase",
 ]
