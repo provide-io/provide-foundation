e22ed507a110ec3b3be5a08dec031b4e57bf9c7c
Tim <code@tim.life>
2025-09-01 11:01:47 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/provide/foundation/utils/__init__.py
+++ src/provide/foundation/utils/__init__.py
@@ -11,11 +11,15 @@ from provide.foundation.utils.parsing import (
     parse_list,
     parse_typed_value,
 )
+from provide.foundation.utils.timing import timed_block
 
 __all__ = [
+    # Parsing utilities
     "auto_parse",
     "parse_bool",
     "parse_dict", 
     "parse_list",
     "parse_typed_value",
+    # Timing utilities
+    "timed_block",
 ]
