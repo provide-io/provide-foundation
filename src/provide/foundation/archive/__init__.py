93943a3fc7e17014fea3b6dca7543c4e3522b833
Tim <code@tim.life>
2025-09-07 17:08:57 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/provide/foundation/archive/__init__.py
+++ src/provide/foundation/archive/__init__.py
@@ -5,21 +5,19 @@ Tools for creating, extracting, and manipulating archives in various formats.
 """
 
 from provide.foundation.archive.base import ArchiveError, BaseArchive
+from provide.foundation.archive.bzip2 import Bzip2Compressor
+from provide.foundation.archive.gzip import GzipCompressor
+from provide.foundation.archive.operations import ArchiveOperations, OperationChain
 from provide.foundation.archive.tar import TarArchive
-
-# Will be added as implemented:
-# from provide.foundation.archive.gzip import GzipCompressor
-# from provide.foundation.archive.zip import ZipArchive
-# from provide.foundation.archive.bzip2 import Bzip2Compressor
-# from provide.foundation.archive.operations import OperationChain, ArchiveOperations
+from provide.foundation.archive.zip import ZipArchive
 
 __all__ = [
     "ArchiveError",
-    "BaseArchive", 
+    "ArchiveOperations",
+    "BaseArchive",
+    "Bzip2Compressor",
+    "GzipCompressor",
+    "OperationChain",
     "TarArchive",
-    # "GzipCompressor",
-    # "ZipArchive",
-    # "Bzip2Compressor",
-    # "OperationChain",
-    # "ArchiveOperations",
+    "ZipArchive",
 ]
