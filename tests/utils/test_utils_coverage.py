be3878b13fa6e8493da4be8e57b6ad7591733d58
Tim <code@tim.life>
2025-09-14 16:47:12 -0700

    🔼⚙️ [skip ci] auto-commit

--- tests/utils/test_utils_coverage.py
+++ tests/utils/test_utils_coverage.py
@@ -8,7 +8,7 @@ from attrs import define, field, fields
 
 from provide.foundation import LoggingConfig, TelemetryConfig, get_hub, logger
 from provide.foundation.utils import timed_block
-from provide.foundation.utils.formatting import format_table, to_camel_case
+from provide.foundation.utils.text_utils import format_table, to_camel_case
 from provide.foundation.utils.parsing import auto_parse, parse_typed_value
 
 
