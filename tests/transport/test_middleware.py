c5000ee6311cb11f71981700d1cf8ecfbf7d7d5f
Tim <code@tim.life>
2025-09-28 10:49:49 -0700

    🔼⚙️ [skip ci] auto-commit

--- tests/transport/test_middleware.py
+++ tests/transport/test_middleware.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import pytest
 
+from provide.testkit import FoundationTestCase
 from provide.foundation.transport.base import Request, Response
 from provide.foundation.transport.middleware import (
     LoggingMiddleware,
