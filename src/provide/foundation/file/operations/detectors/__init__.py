ea5b9a1a27b840ffc6016b4fce1dfd6f1b1652a7
Tim <code@tim.life>
2025-09-30 19:35:04 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/provide/foundation/file/operations/detectors/__init__.py
+++ src/provide/foundation/file/operations/detectors/__init__.py
@@ -1,13 +1,15 @@
-"""File operation detectors."""
+"""File operation detectors - Public API."""
 
 from __future__ import annotations
 
+from .orchestrator import OperationDetector
 from .atomic import AtomicOperationDetector
 from .batch import BatchOperationDetector
 from .simple import SimpleOperationDetector
 from .temp import TempPatternDetector
 
 __all__ = [
+    "OperationDetector",
     "AtomicOperationDetector",
     "BatchOperationDetector",
     "SimpleOperationDetector",
