0fc9251813eff9914ce1b30784cb9fc2c349c487
Tim <code@tim.life>
2025-10-02 19:27:17 -0700

    🔼⚙️ [skip ci] auto-commit

--- tests/utils/test_rate_limiting_coverage.py
+++ tests/utils/test_rate_limiting_coverage.py
@@ -15,6 +15,12 @@ from provide.foundation.utils.rate_limiting import TokenBucketRateLimiter
 class TestTokenBucketRateLimiter(FoundationTestCase):
     """Test TokenBucketRateLimiter class."""
 
+    def setup_method(self) -> None:
+        """Set up test environment."""
+        super().setup_method()
+        # Create controlled time for all tests
+        self.get_time, self.advance_time, self.fake_sleep, self.fake_async_sleep = make_controlled_time()
+
     def test_init_valid_parameters(self) -> None:
         """Test initialization with valid parameters."""
         limiter = TokenBucketRateLimiter(capacity=10, refill_rate=5)
