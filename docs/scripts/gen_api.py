83b39fdd9b47b9d1acf4e0b00d5ad886415d9ca7
Tim <code@tim.life>
2025-10-31 20:00:00 -0700

    chore(docs): update API reference generation scripts and MkDocs config

---
+++ docs/scripts/gen_api.py
@@ -0,0 +1,8 @@
+"""Wrapper script to invoke provide.foundry API reference generation."""
+
+from __future__ import annotations
+
+from provide.foundry.docs import generate_reference_pages
+
+# Execute the generation
+generate_reference_pages()
